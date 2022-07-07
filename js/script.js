7215d1f242e0a2fb1fc1cc8f5ba84d3374f0bc79
Nikolay Vinogradov <was.bic@gmail.com>
2022-07-07 14:59:14 +0300

    Выполнено задание. Урок 13

---
+++ js/script.js
@@ -0,0 +1,59 @@
+'use strict';
+
+const todoControl = document.querySelector('.todo-control');
+const headerInput = document.querySelector('.header-input');
+const todoList = document.querySelector('.todo-list');
+const todoCompleted = document.querySelector('.todo-completed');
+
+const toDoData = localStorage.getItem('toDoData') ?  JSON.parse(localStorage.getItem("toDoData")) : [];
+
+const render = function () {
+  todoList.innerHTML = '';
+  todoCompleted.innerHTML = '';
+  localStorage.setItem("toDoData", JSON.stringify(toDoData));
+
+  toDoData.forEach(function (item) {
+    const li = document.createElement('li');
+
+    li.classList.add('todo-item');
+    
+    li.innerHTML = '<span class="text-todo">' + item.text + '</span>'+
+    '<div class="todo-buttons">' +
+      '<button class="todo-remove"></button>' +
+      '<button class="todo-complete"></button>' +
+    '</div>';    
+
+    if(item.completed) {
+      todoCompleted.append(li);
+    } else {
+      todoList.append(li);
+    }
+    li.querySelector('.todo-complete').addEventListener('click', function () {
+      item.completed = !item.completed;
+      render();
+    });
+    li.querySelector('.todo-remove').addEventListener('click', function () {
+      li.remove('todo-item');
+      toDoData.splice(0, 1);
+      render();
+    });
+    
+    
+  });
+  
+};
+todoControl.addEventListener('submit', function (event) {
+  event.preventDefault();
+  if(headerInput.value !== ''){
+    const newToDo = {
+      text: headerInput.value,
+      completed: false
+    };
+    
+    toDoData.push(newToDo);
+    headerInput.value = '';
+    render();
+  }
+});
+console.log(toDoData);
+render();
